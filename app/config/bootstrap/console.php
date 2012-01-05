321fe4d584a246135e09626a5569f2e58d8bc046
gwoo <gwoohoo@gmail.com>
2012-01-04 17:58:48 -0800

    adding filter to bootstrap/console.php for setting environment. commenting out previous filer used for colorizing.

--- app/config/bootstrap/console.php
+++ app/config/bootstrap/console.php
@@ -7,13 +7,27 @@
  */
 
 use lithium\console\Dispatcher;
+use lithium\core\Environment;
 
-Dispatcher::applyFilter('_call', function($self, $params, $chain) {
-	$params['callable']->response->styles(array(
-		'heading' => '\033[1;30;46m'
-	));
-	return $chain->next($self, $params, $chain);
+/**
+ * This filter sets the environment based on the current request. By default, `$request->env`, for
+ * example in the command `li3 help --env=production`, is used to determine the environment.
+ *
+ */
+Dispatcher::applyFilter('run', function($self, $params, $chain) {
+	Environment::set($params['request']);
 });
 
+/**
+ * This filter will convert {:heading} to the specified color codes. This is useful for colorizing
+ * output and creating different sections.
+ *
+ */
+// Dispatcher::applyFilter('_call', function($self, $params, $chain) {
+// 	$params['callable']->response->styles(array(
+// 		'heading' => '\033[1;30;46m'
+// 	));
+// 	return $chain->next($self, $params, $chain);
+// });
 
 ?>
