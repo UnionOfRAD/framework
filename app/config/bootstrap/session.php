72d66738b66f2dc2291250ac38d6af97fb3b7ba0
gwoo <gwoohoo@gmail.com>
2012-01-04 17:46:20 -0800

    Manually setting session.name in session.php.

--- app/config/bootstrap/session.php
+++ app/config/bootstrap/session.php
@@ -9,12 +9,15 @@
 /**
  * This configures your session storage. The Cookie storage adapter must be connected first, since
  * it intercepts any writes where the `'expires'` key is set in the options array.
+ * The default name is based on the lithium app path. Remember, if your app is numeric or has
+ * special characters you might want to use Inflector::slug() or set this manually.
  */
 use lithium\storage\Session;
 
+$name = basename(LITHIUM_APP_PATH);
 Session::config(array(
-	// 'cookie' => array('adapter' => 'Cookie'),
-	'default' => array('adapter' => 'Php')
+	// 'cookie' => array('adapter' => 'Cookie', 'name' => $name),
+	'default' => array('adapter' => 'Php', 'session.name' => $name)
 ));
 
 /**
