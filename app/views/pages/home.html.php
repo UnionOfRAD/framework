d524e869b73077ef7ce013780cfb4c2f16c13df0
Mehdi Lahmam B <mehdi@lahmam.com>
2012-05-18 17:50:59 +0200

    Fix checking the setting of `magic_quotes_gpc()` for PHP 5.4.
    Conflicts:
    
    	app/views/pages/home.html.php
    
    Signed-off-by: David Persson <davidpersson@gmx.de>
    
    This overlaps with b34f23d8ee62014b06bfc7d1b23dd45fbd35c91e.

--- app/views/pages/home.html.php
+++ app/views/pages/home.html.php
@@ -65,7 +65,7 @@ $checks = array(
 		);
 	},
 	'magicQuotes' => function() use ($notify) {
-		if (get_magic_quotes_gpc() === 0 || get_magic_quotes_gpc() === false) {
+		if (!get_magic_quotes_gpc()) {
 			return;
 		}
 		return $notify(
