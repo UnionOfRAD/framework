eb5467e2450e48afa97fa96a2732f7a42c86d46a
David Persson <davidpersson@gmx.de>
2014-01-13 13:07:36 +0100

    Refactor caching bootstrap; updating documentation.

--- app/config/bootstrap/cache.php
+++ app/config/bootstrap/cache.php
@@ -6,10 +6,6 @@
  * @license       http://opensource.org/licenses/bsd-license.php The BSD License
  */
 
-/**
- * This file creates a default cache configuration using the most optimized adapter available, and
- * uses it to provide default caching for high-overhead operations.
- */
 use lithium\storage\Cache;
 use lithium\core\Libraries;
 use lithium\core\Environment;
@@ -17,41 +13,61 @@ use lithium\action\Dispatcher;
 use lithium\storage\cache\adapter\Apc;
 
 /**
- * If APC is not available and the cache directory is not writeable, bail out. This block should be
- * removed post-install, and the cache should be configured with the adapter you plan to use.
+ * Configuration
+ *
+ * Most of this code is for getting you up and running only, and should be replaced with
+ * a hard-coded configuration, based on the cache(s) you plan to use.
+ *
+ * We create a default cache configuration using the most optimized adapter available, and
+ * use it to provide default caching for high-overhead operations. If APC is not available
+ * and we can't degrade to file based caching, bail out.
+ *
+ * @see lithium\storage\Cache
+ * @see lithium\storage\cache\adapters
+ * @see lithium\storage\cache\strategies
  */
 $cachePath = Libraries::get(true, 'resources') . '/tmp/cache';
 
-if (!(($apcEnabled = Apc::enabled()) || PHP_SAPI === 'cli') && !is_writable($cachePath)) {
+if (!(($apc = Apc::enabled()) || PHP_SAPI === 'cli') && !is_writable($cachePath)) {
 	return;
 }
+Cache::config(array(
+	'default' => array(
+		'adapter' => $apc ? 'Apc' : 'File',
+		'strategies' => $apc ? array() : array('Serializer')
+	)
+));
 
 /**
- * This configures the default cache, based on whether ot not APC user caching is enabled. If it is
- * not, file caching will be used. Most of this code is for getting you up and running only, and
- * should be replaced with a hard-coded configuration, based on the cache(s) you plan to use.
+ * Apply
+ *
+ * Applies caching to neuralgic points of the framework but only when we are running
+ * in production. This is also a good central place to add your own caching rules.
+ *
+ * Here we cache paths for auto-loaded and service-located classes.
+ *
+ * @see lithium\core\Environment
+ * @see lithium\core\Libraries
  */
-Cache::config(array('default' => $apcEnabled ? array('adapter' => 'Apc') : array(
-	'adapter' => 'File', 'strategies' => array('Serializer')
-)));
+if (!Environment::is('production')) {
+	return;
+}
+$cacheScope = md5(LITHIUM_APP_PATH);
 
 /**
  * Caches paths for auto-loaded and service-located classes when in production.
  */
-Dispatcher::applyFilter('run', function($self, $params, $chain) {
-	if (!Environment::is('production')) {
-		return $chain->next($self, $params, $chain);
-	}
-	$key = md5(LITHIUM_APP_PATH) . '.core.libraries';
+Dispatcher::applyFilter('run', function($self, $params, $chain) use ($cacheScope) {
+	$cacheKey = "{$cacheScope}.core.libraries";
 
-	if ($cache = Cache::read('default', $key)) {
-		$cache = (array) $cache + Libraries::cache();
-		Libraries::cache($cache);
+	if ($cached = Cache::read('default', $cacheKey)) {
+		$cached = (array) $cached + Libraries::cache();
+		Libraries::cache($cached);
 	}
 	$result = $chain->next($self, $params, $chain);
 
-	if ($cache != Libraries::cache()) {
-		Cache::write('default', $key, Libraries::cache(), '+1 day');
+	if ($cached != ($data = Libraries::cache())) {
+		Cache::write('default', $cacheKey, $data, '+1 day');
 	}
 	return $result;
 });
